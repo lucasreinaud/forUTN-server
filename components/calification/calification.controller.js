111fe58b6b6ebd7694d69493e4d75c75c6dd6904
davidmarco12 <marcolindavid1@gmail.com>
2021-09-07 15:56:49 -0300

    Testings creados y Api mejorada

--- components/calification/calification.controller.js
+++ components/calification/calification.controller.js
@@ -3,7 +3,8 @@ const {Sequelize, QueryTypes, Op} = require('sequelize');
 
 
 const {
-    Calification
+    Calification,
+    Input
 } = require('../../database')
 
 
@@ -14,6 +15,40 @@ router.get('/', async (req, res) => {
         res.status(200).json(califications);  
     } catch (error) {
         console.log("Error en el GET CALIFICACIONES", error);
+    }
+});
+
+//Calificaciones por id publicacion
+router.get('/:id', async (req, res) => {
+    try {
+        console.log("calificaciones");
+        const califications = await Calification.findAll({
+            where: {
+                identrada : req.params.id
+            }
+        }); 
+        res.status(200).json(califications);  
+    } catch (error) {
+        console.log("Error en el GET CALIFICACIONES", error);
+    }
+});
+
+
+router.post('/', async (req, res) => {
+    try {
+        const { identrada, idusuario, tipoclasificacion } = req.body;
+        const inputCreated = await Input.create({
+            identrada,
+            idusuario,
+            tipoclasificacion
+            });
+
+        res.status(200).json({
+            status: 200,
+            message:'Calificacion creada',
+        });          
+    } catch (e) {
+        error(res,400,'error en el post calification', e)
     }
 });
 
