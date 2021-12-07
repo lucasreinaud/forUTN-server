9c620f6dec6a624dadd4a70db8fdc053790cd2fc
Lucas Reinaud <lucasreinaud@gmail.com>
2021-12-07 18:12:27 -0300

    avances

--- bin/error.js
+++ bin/error.js
@@ -2,7 +2,7 @@ const error = (res, status, message, err) => {
     var today = new Date();
     var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    
+    console.log(err);
     return res.status(status).json({
         response: 'ERROR',
         message : "["+ date + " | " + time + "] " + message ,
